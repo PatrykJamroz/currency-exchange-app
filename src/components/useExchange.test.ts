5b49ab037b7b445efef8b6edd1e05481f5dd6220
Patryk J <jamroz.patryk@gmail.com>
2021-01-30 14:26:25 +0100

    tests

--- src/components/useExchange.test.ts
+++ src/components/useExchange.test.ts
@@ -10,4 +10,32 @@ describe("useExchange", () => {
 
     expect(result.current.inputOne).toBe(15);
   });
+  it("change inputTwo", () => {
+    const { result } = renderHook(() => useExchange());
+    act(() => {
+      result.current.inputTwoChange({ currentTarget: { value: "10" } } as any);
+    });
+
+    expect(result.current.inputTwo).toBe(10);
+  });
+
+  it("change currencyOne", () => {
+    const { result } = renderHook(() => useExchange());
+    act(() => {
+      result.current.currencyOneChange({
+        currentTarget: { value: "EUR" },
+      } as any);
+    });
+    expect(result.current.currencyOne).toBe("EUR");
+  });
+
+  it("change currencyTwo", () => {
+    const { result } = renderHook(() => useExchange());
+    act(() => {
+      result.current.currencyTwoChange({
+        currentTarget: { value: "USD" },
+      } as any);
+    });
+    expect(result.current.currencyTwo).toBe("USD");
+  });
 });
